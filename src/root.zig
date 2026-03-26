883f4de4b69a07cfa228a2386a2d798143477337
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-26 21:31:39 +0900

    feat: add element table with VDW radii and atom flags

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,7 @@
 //! zreduce: Hydrogen placement and optimization for mmCIF structures.
 
 pub const math = @import("math.zig");
+pub const element = @import("element.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
