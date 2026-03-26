4eee4bb3fd8bceffce4372b87107d31803a390db
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-26 22:23:42 +0900

    feat: add hardcoded H placement plans for 20 standard amino acids

--- src/place.zig
+++ src/place.zig
@@ -1,6 +1,7 @@
 //! Place module: hydrogen placement geometry and utilities.
 
 pub const geometry = @import("place/geometry.zig");
+pub const standard = @import("place/standard.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
