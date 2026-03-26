b82ca1ffe66934cfb6d5ae1eb5af4feae38cf814
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-26 23:41:37 +0900

    feat: add OH/SH/NH3+/methyl rotation movers

--- src/optimize/optimize.zig
+++ src/optimize/optimize.zig
@@ -3,6 +3,11 @@
 pub const dot_sphere = @import("dot_sphere.zig");
 pub const DotSphere = dot_sphere.DotSphere;
 pub const scorer = @import("scorer.zig");
+pub const mover = @import("mover.zig");
+pub const Mover = mover.Mover;
+pub const MoverKind = mover.MoverKind;
+pub const Orientation = mover.Orientation;
+pub const rotator = @import("rotator.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
