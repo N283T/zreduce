d34c47415a21cd44a38d5603ed7ae9eb8d526bf4
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-26 23:39:19 +0900

    feat: add dot-sphere bump/H-bond scorer
    Implements dot-sphere probe scoring for atom contacts, classifying
    each probe contact as VDW contact, H-bond, or steric clash.

--- src/optimize/optimize.zig
+++ src/optimize/optimize.zig
@@ -2,6 +2,7 @@
 
 pub const dot_sphere = @import("dot_sphere.zig");
 pub const DotSphere = dot_sphere.DotSphere;
+pub const scorer = @import("scorer.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
