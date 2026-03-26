c0ea9378c68218d5ee61e69920bd86701a597333
N283T <w255508b@yokohama-cu.ac.jp>
2026-03-26 21:26:09 +0900

    feat: initial project scaffold with build system

---
+++ src/root.zig
@@ -0,0 +1,7 @@
+//! zreduce: Hydrogen placement and optimization for mmCIF structures.
+
+// Modules will be added as they are implemented.
+
+test {
+    @import("std").testing.refAllDecls(@This());
+}
